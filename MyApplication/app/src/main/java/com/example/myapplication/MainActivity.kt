8507932693448f4f6eee112fc20812f692e254d4
김경민 <rudals9686@naver.com>
2022-08-06 16:33:51 +0900

    22

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,18 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.Toast
+import coil.load
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val imgView = findViewById<ImageView>(R.id.iv)
+//        imgView.setImageResource(R.drawable.img)
+        imgView.load(R.drawable.img)
 
-        val button = findViewById<Button>(R.id.button)
-
-        button.setOnClickListener{
-            Toast.makeText(this, "김경민", Toast.LENGTH_SHORT).show()
-            Log.d("student ID","2019041703")
-        }
     }
 }
